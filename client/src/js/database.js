b4b0ebdac005e0ee2a9543e97d134327fa5d0729
minkiko@gmail.com <minkiko@gmail.com>
2023-09-23 10:08:22 -0500

    console log displays updated text entry has been saved to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,6 +17,9 @@ export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log("Updated text entry has been saved to the database!", result);
 };
   
 
