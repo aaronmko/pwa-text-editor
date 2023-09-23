246074abcd9d8f2a69faa5b4e47c0eddaf8233c7
minkiko@gmail.com <minkiko@gmail.com>
2023-09-23 10:07:48 -0500

    storing jate as a variable store

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,6 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
 };
   
 
